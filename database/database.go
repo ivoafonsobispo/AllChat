4e58aef7b1cee72456ac90c4a1aa60c38996953c
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-04-07 13:30:25 +0100

    gcp documentation connect to db

--- database/database.go
+++ database/database.go
@@ -1,46 +1,64 @@
 package database
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
+	"net"
 	"os"
 
-	_ "github.com/lib/pq"
+	"cloud.google.com/go/cloudsqlconn"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 )
 
 type DB struct {
 	*sql.DB
 }
 
-func InitDB() *DB {
+func InitDB() (*sql.DB, error) {
 	mustGetenv := func(k string) string {
 		v := os.Getenv(k)
 		if v == "" {
-			log.Fatalf("%s environment variable not set", k)
+			log.Fatalf("Fatal Error in connect_connector.go: %s environment variable not set.\n", k)
 		}
 		return v
 	}
 
 	var (
-		dbUser         = mustGetenv("DB_USER")
-		dbPwd          = mustGetenv("DB_PASSWORD")
-		unixSocketPath = "/cloudsql/" + mustGetenv("INSTANCE_CONNECTION_NAME")
-		dbName         = mustGetenv("DB_NAME")
+		dbUser                 = mustGetenv("DB_USER")
+		dbPwd                  = mustGetenv("DB_PASS")
+		dbName                 = mustGetenv("DB_NAME")
+		instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME")
+		usePrivate             = os.Getenv("PRIVATE_IP")
 	)
 
-	dbURI := fmt.Sprintf("user=%s password=%s database=%s host=%s",
-		dbUser, dbPwd, dbName, unixSocketPath)
-
-	dbPool, err := sql.Open("postgres", dbURI)
+	dsn := fmt.Sprintf("user=%s password=%s database=%s", dbUser, dbPwd, dbName)
+	config, err := pgx.ParseConfig(dsn)
 	if err != nil {
-		log.Fatalf("sql.Open: %v", err)
+		return nil, err
 	}
 
-	_, err = dbPool.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT UNIQUE, password TEXT)")
-	if err != nil {
-		log.Fatalf("Failed to create table: %v", err)
+	var opts []cloudsqlconn.Option
+	if usePrivate != "" {
+		opts = append(opts, cloudsqlconn.WithDefaultDialOptions(cloudsqlconn.WithPrivateIP()))
 	}
 
-	return &DB{dbPool}
+	d, err := cloudsqlconn.NewDialer(context.Background(), opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	config.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
+		return d.Dial(ctx, instanceConnectionName)
+	}
+
+	dbURI := stdlib.RegisterConnConfig(config)
+	dbPool, err := sql.Open("pgx", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
+	}
+
+	return dbPool, nil
 }

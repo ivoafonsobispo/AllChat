a29cf463bf78e1bf2cc77d2e48ce00ef81b56b50
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-04-07 17:25:08 +0100

    gcp docs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,20 +12,24 @@ import (
 
 func main() {
 	// Initialize database
-	db := database.InitDB()
+	db, err := database.InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 	// Create a Router
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/users", handlers.GetUsers(db.DB)).Methods("GET")
-	router.HandleFunc("/api/users/{id}", handlers.GetUser(db.DB)).Methods("GET")
+	router.HandleFunc("/api/users", handlers.GetUsers(db)).Methods("GET")
+	router.HandleFunc("/api/users/{id}", handlers.GetUser(db)).Methods("GET")
 
-	router.HandleFunc("/api/users", handlers.CreateUser(db.DB)).Methods("POST")
-	router.HandleFunc("/api/users/login", handlers.Login(db.DB)).Methods("POST")
+	router.HandleFunc("/api/users", handlers.CreateUser(db)).Methods("POST")
+	router.HandleFunc("/api/users/login", handlers.Login(db)).Methods("POST")
 
-	router.HandleFunc("/api/users/{id}", handlers.UpdateUser(db.DB)).Methods("PUT")
+	router.HandleFunc("/api/users/{id}", handlers.UpdateUser(db)).Methods("PUT")
 
-	router.HandleFunc("/api/users/{id}", handlers.DeleteUser(db.DB)).Methods("DELETE")
+	router.HandleFunc("/api/users/{id}", handlers.DeleteUser(db)).Methods("DELETE")
 
 	// Handle the JSON
 	enhancedRouter := middlewares.EnableCORS(middlewares.JSONContentTypeMiddleware(router))
--- database/database.go
+++ database/database.go
@@ -1,31 +1,61 @@
 package database
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"log"
+	"net"
+	"os"
 
-	_ "github.com/lib/pq"
+	"cloud.google.com/go/cloudsqlconn"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 )
 
-type DB struct {
-	*sql.DB
-}
+func InitDB() (*sql.DB, error) {
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Fatal Error: %s environment variable not set.\n", k)
+		}
+		return v
+	}
 
-func InitDB() *DB {
-	// Hardcoded connection string
-	dbURL := "/cloudsql/chat-app-419508:us-central1:accountsdb"
+	dbUser := mustGetenv("DB_USER")
+	dbPwd := mustGetenv("DB_PASS")
+	dbName := mustGetenv("DB_NAME")
+	instanceConnectionName := mustGetenv("INSTANCE_CONNECTION_NAME")
 
-	// Connect to DB using the hardcoded connection string
-	db, err := sql.Open("postgres", dbURL)
+	dsn := fmt.Sprintf("user=%s password=%s database=%s", dbUser, dbPwd, dbName)
+	config, err := pgx.ParseConfig(dsn)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+
+	var opts []cloudsqlconn.Option
+	d, err := cloudsqlconn.NewDialer(context.Background(), opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	config.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
+		return d.Dial(ctx, instanceConnectionName)
+	}
+
+	dbURI := stdlib.RegisterConnConfig(config)
+	dbPool, err := sql.Open("pgx", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
 	}
 
 	// Create table if it doesn't exist with id, username, and password
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT UNIQUE, password TEXT)")
+	_, err = dbPool.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT UNIQUE, password TEXT)")
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	return &DB{db}
+	log.Println("Connected to Cloud SQL database")
+
+	return dbPool, nil
 }

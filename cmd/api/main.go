3d69e02aec69fcca68c55467efff21c36951ad3d
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-04-07 12:42:01 +0100

    changed port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -30,5 +30,5 @@ func main() {
 	enhancedRouter := middlewares.EnableCORS(middlewares.JSONContentTypeMiddleware(router))
 
 	// Start Server
-	http.ListenAndServe(":8000", enhancedRouter)
+	http.ListenAndServe(":8002", enhancedRouter)
 }

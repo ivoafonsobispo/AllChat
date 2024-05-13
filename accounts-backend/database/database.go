b9885ba4a6b0c955a1df78ed16ab362d5a291f04
Imeguras <imeguras@cock.li>
2024-05-13 19:15:04 +0100

    Seperated sql script from code

--- accounts-backend/database/database.go
+++ accounts-backend/database/database.go
@@ -18,20 +18,14 @@ func InitDB() *DB {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// An ORM would make the cood cleaner, maybe export this into an sql script?
+	//open ../sql/Scheme.sql and execute it
+	// get string from sql/Scheme.sql
+	scheme, err := os.ReadFile("/Scheme.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
 	
-	// Create table if doesnt exist with id, username and password
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT UNIQUE, password TEXT,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP, deleted BOOLEAN DEFAULT FALSE)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	//
-	// CREATE TABLE IF NOT EXISTS groups (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), name TEXT)
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS groups (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), name TEXT, deleted BOOLEAN DEFAULT FALSE, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS rel_user_group (user_id INT, group_id UUID, deleted BOOLEAN DEFAULT FALSE, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (user_id, group_id), FOREIGN KEY (user_id) REFERENCES users(id), FOREIGN KEY (group_id) REFERENCES groups(id))")
+	_, err = db.Exec(string(scheme))
 	if err != nil {
 		log.Fatal(err)
 	}

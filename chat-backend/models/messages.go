bd44aeff08161b86e2a58207bd621b93bbdddd14
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2024-05-20 20:01:39 +0100

    fix: Chat Models

--- chat-backend/models/messages.go
+++ chat-backend/models/messages.go
@@ -2,18 +2,18 @@ package models
 
 import "time"
 
+type Message struct {
+	Content   string    `json:"content"`
+	Username  string    `json:"username"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
 type ReceivedMessage struct {
 	GroupId string  `json:"groupid"`
 	Message Message `json:"message"`
 }
 
-type RetrieveMessages struct {
+type GroupMessages struct {
 	GroupId  string    `json:"groupid"`
 	Messages []Message `json:"messages"`
-}
-
-type Message struct {
-	Content   string    `json:"content"`
-	Username  string    `json:"username"`
-	Timestamp time.Time `json:"timestamp"`
 }
